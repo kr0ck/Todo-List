547158e53a708469041d19fb43349caa0d92f99e
kr0ck <hebertcampos@hotmail.com.br>
2021-01-29 16:53:42 -0300

    Gerenciando o estado na page

--- lib/app/views/home_page.dart
+++ lib/app/views/home_page.dart
@@ -1,13 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/app/controllers/home_controller.dart';
 
 class HomePage extends StatelessWidget {
+  _success() {
+    return ListView.builder(
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('item $index'),
+        );
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {},
+        child: Text('Tentar novamente'),
+      ),
+    );
+  }
+
+  _loading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  _start() {
+    return Container();
+  }
+
+  stateManagement(HomeState state) {
+    switch (state) {
+      case HomeState.start:
+        return _start();
+      case HomeState.loading:
+        return _loading();
+      case HomeState.error:
+        return _error();
+      case HomeState.success:
+        return _success();
+      default:
+        return _start();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('List Todo\'s'),
       ),
-      body: Container(),
+      body: stateManagement(HomeState.loading),
     );
   }
 }
