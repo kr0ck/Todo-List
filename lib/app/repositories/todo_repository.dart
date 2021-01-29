e558ae4cb612d1b89d7b1952286d682b83b892cc
kr0ck <hebertcampos@hotmail.com.br>
2021-01-29 16:08:56 -0300

    Refatorando codigo do repository

--- lib/app/repositories/todo_repository.dart
+++ lib/app/repositories/todo_repository.dart
@@ -5,23 +5,11 @@ class TodoRepository {
   Dio dio;
   final url = 'https://jsonplaceholder.typicode.com/todos';
 
-  TodoRepository([Dio client]) {
-    if (client == null) {
-      this.dio = Dio();
-    } else {
-      this.dio = client;
-    }
-  }
+  TodoRepository([Dio client]) : dio = client ?? Dio();
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
-
-    List<TodoModel> todos = [];
-    for (var json in list) {
-      final todo = TodoModel.fromJson(json);
-      todos.add(todo);
-    }
-    return todos;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
